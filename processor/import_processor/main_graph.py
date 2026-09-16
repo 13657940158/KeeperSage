c6c75bb47a664de9eb8566ea045952ce18dc796c
links <2476456123@qq.com>
2026-09-16 18:50:51 +0800

    to dev_1

--- processor/import_processor/main_graph.py
+++ processor/import_processor/main_graph.py
@@ -1,8 +1,10 @@
+import json
 import logging
 
 from langgraph.constants import END
 from langgraph.graph import StateGraph
 
+from processor.import_processor.base import setup_logging
 from processor.import_processor.nodes.node_bge_embedding import NodeBGEEmbedding
 from processor.import_processor.nodes.node_document_split import NodeDocumentSplit
 from processor.import_processor.nodes.node_entry import NodeEntry
@@ -18,7 +20,7 @@ class KBImportWorkflow:
         self._compiled_graph = None#实例属性
 
     @property
-    def graph(self):  # 可以通过KBImportWorkflow.graph使用
+    def graph(self):  # 可以通过KBImportWorkflow.graph使用，类方法
         """
         返回图实例
         """
@@ -29,14 +31,16 @@ class KBImportWorkflow:
 
 
 
-    @staticmethod
+    @staticmethod#工具方法
     def route_after_entry(state: ImportGraphState):
         # 这里由入口方法node_entry来告诉这个方法是pdf还是md
         if state.get("is_pdf_read_enabled"):
             return "node_pdf_to_md"
         elif state.get("is_md_read_enabled"):
             return "node_md_img"
-        else: END
+        else:
+            logging.info("route_after_entry路由器:未指定导入文件类型")
+            return END
 
 
 
@@ -72,8 +76,18 @@ class KBImportWorkflow:
         graph.add_edge("node_bge_embedding", "node_import_milvus")
         graph_compile=graph.compile()
         return graph_compile
-
+    #run函数
+    def run(self,state: ImportGraphState,stream:bool=False):
+        if stream:
+            return self.graph.stream(state,stream_mode="values")
+        else:
+            return self.graph.invoke(state)
 if __name__ == "__main__":
+    # 启用日志
+    setup_logging()
     workflow = KBImportWorkflow()
-    graph = workflow.graph
-    print(graph)
+    init_state = {"import_file_path": r"D:\doc\H3C.md"}#文件路径
+    for event in workflow.run(init_state,stream=True):
+        print(f"state:{event}")
+    # final_state = workflow.run(init_state,stream=False)
+    # print(json.dumps(final_state,ensure_ascii=False,indent=4))
--- processor/import_processor/nodes/node_pdf_to_md.py
+++ processor/import_processor/nodes/node_pdf_to_md.py
@@ -1,4 +1,11 @@
-from processor.import_processor.base import BaseNode
+import json
+from pathlib import Path
+from plistlib import dumps
+
+import logging
+
+from processor.import_processor.base import BaseNode, setup_logging
+from processor.import_processor.exceptions import StateFieldError, FileProcessingError
 from processor.import_processor.state import ImportGraphState
 
 
@@ -10,5 +17,60 @@ class NodePDFToMD(BaseNode):
     name = "node_pdf_to_md"
 
     def process(self, state: ImportGraphState):
+        logging.info(f"{self.name}节点开始执行...")
+        #检查和获取相关的参数
+        #校验PDF路径和输出目录
+        pdf_path_obj, output_dir_obj = self._step_1_validate_paths(state)
 
+        # 获取上传链接并删除更换文件到mineru服务器
+        zip_url = self._step_2_upload_and_poll(pdf_path_obj)
+
+        #下载zip压缩文件并解压改名
+        md_path = self._step_3_download_and_extract(zip_url, output_dir_obj, pdf_path_obj.stem)
+
+        #读取md的内容
+        with open(md_path, "r", encoding="utf-8") as md_file:
+            md_content = md_file.read()
+
+        #设置state结果
+        state["md_content"] = md_content
+        state["md_path"] = md_path
         return state
+
+    def _step_1_validate_paths(self, state:ImportGraphState):
+        #校验路径
+        pdf_path = state.get("pdf_path")#这是上一个节点的,上一个节点传回给state了
+        if not pdf_path:
+            raise StateFieldError(field_name="pdf_path",expected_type=str)
+        file_dir = state.get("file_dir")#这里是导出文件的路径
+        if not file_dir:
+            raise StateFieldError(field_name="file_dir",expected_type=str)
+        #封装路径,封装成对象
+        pdf_path_obj = Path(pdf_path)
+        output_dir_obj = Path(file_dir)
+        #判断文件是否存在
+        if not pdf_path_obj.exists():
+            raise FileProcessingError(message=f"输入文件不存在:{pdf_path}")
+        if not output_dir_obj.exists():
+            raise FileProcessingError(message=f"输出的目录不存在:{output_dir_obj}")
+
+        return  pdf_path_obj, output_dir_obj
+
+    def _step_2_upload_and_poll(self, pdf_path_obj: Path):
+        logging.info(f"_step_2_upload_and_poll上传文件到mineru服务器...")
+        return "下载url"
+
+    def _step_3_download_and_extract(self, zip_url: str, output_dir_obj: Path, pdf_stem: str) -> str:
+        logging.info(f"_step_3_download_and_extract下载并解压改名")
+        return "md_path"
+#测试
+if __name__ == "__main__":
+    setup_logging()
+    init_state={
+        "pdf_path":r"D:\doc\H3C.pdf",
+        "file_dir":r"D:\doc\output",
+    }
+    node = NodePDFToMD()
+    result = node.process(init_state)#也可省略.proess,bsae.py文件中有默认执行
+    json.dumps(result, ensure_ascii=False, indent=4)
+    print(dumps)
